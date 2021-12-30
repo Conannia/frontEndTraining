b84da99f894e96246baaf8cb4379475d82fc9866
Conannia <conannia17@gmail.com>
2021-12-30 20:18:36 +0700

    Redux-Thunk dan counter

---
+++ todo_list/todo_list/src/app.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavigationBar from './component/NavBar'
+import TodoPages from './pages/TodoPages'
+
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <NavigationBar/>
+                <TodoPages/>
+            </div>
+        )
+    }
+}
+
+export default App
